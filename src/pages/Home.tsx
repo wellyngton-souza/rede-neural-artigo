0fd3efafbdf8d3da3cbceb41bf8a124d6a0e8b31
wellyngton-souza <wellyngtonteixeira12@gmail.com>
2023-10-20 01:41:43 -0300

    feat: apple system

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,9 @@
 import NavBar from '../components/navbar';
 import Rodape from '../components/footer';
 import { Link } from 'react-router-dom';
+import ClassificarFruta from '../content/classificaFruta';
 import RouteContent from '../routes/Content';
+import GameDino from '../content/gameDino/game';
 
 const Home = () =>{
   return (
@@ -16,6 +18,8 @@ const Home = () =>{
                   <div className="min-w-96 p-8 grow">
                     <h1 className="text-xl pb-4">Redes Neurais</h1>
                     <RouteContent />
+                    <ClassificarFruta />
+                    <GameDino />
                   </div>
                   <div className="p-8">
                     <h1 className="text-xl pb-4">Redes Neurais</h1>
